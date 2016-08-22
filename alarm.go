cd9ad59904aa1327a70077da581dc2f70f7a14c2
tsuchi83 <tsuchi83@gmail.com>
2016-08-22 21:06:46 +0900

    first commit

--- alarm.go
+++ alarm.go
@@ -6,7 +6,6 @@ import (
 	"os/exec"
 )
 
-// アラーム音鳴らす
 func start() {
 	//out, err := exec.Command("sh", "-c", "mpg321 -l 0 SIREN.mp3 > /dev/null &").Output()
 	out, err := exec.Command("play", "-t", "mp3", "ALARM.mp3").Output()
